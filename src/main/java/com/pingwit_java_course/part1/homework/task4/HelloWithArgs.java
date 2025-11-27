335cd6501e545f4048061b84bbfe65da00aba067
Ivan Khomich <khomichivan03@gmail.com>
2025-11-27 18:49:02 +0100

    [Fixed] -Deleted space after merge conflict example

--- src/main/java/com/pingwit_java_course/part1/homework/task4/HelloWithArgs.java
+++ src/main/java/com/pingwit_java_course/part1/homework/task4/HelloWithArgs.java
@@ -2,6 +2,6 @@ package com.pingwit_java_course.part1.homework.task4;
 
 public class HelloWithArgs {
     public static void main(String[] args) {
-        System.out.println("Hello," + args[0]) ;
+        System.out.println("Hello," + args[0]);
     }
 }
